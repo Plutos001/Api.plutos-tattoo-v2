157bf92c5f39e6d5cb55dacad8a41bc19f31b101
Plutos001 <nrdo23092001@gmail.com>
2026-05-20 06:36:07 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "API PLUTOS TATTOO ONLINE 💎⚙️"
+    return "API PLUTOS TATTOO V2 ONLINE 💎⚙️"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
